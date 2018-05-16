d588c74f4ef9f587c4eab066cbd35fd2de764eac
liversquatch <sheldonm1@go.lisd.net>
2018-05-16 15:12:32 -0500

    Work 5/16 Part 1

---
+++ Project Andromeda/Assets/Scripts/Background.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+    public float speed;
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
