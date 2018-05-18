6b47adbcdb07c7df68ac5edce3e58f731b4c32b4
habicha <habicha1@go.lisd.net>
2018-05-18 15:00:09 -0500

    Spawn random objects from list

--- Project Andromeda/Assets/Scripts/SpawnObjects.cs
+++ Project Andromeda/Assets/Scripts/SpawnObjects.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnObjects {
+public class SpawnObjects : MonoBehaviour {
+
+	public SpawnableObjects SpawnableObjectList;
+	public Transform ObjectParent;
 
 	public void Spawn() {
 		
-		
+		var newObject = Instantiate(SpawnableObjectList.Random().Object, ObjectParent);
 	}
 }
