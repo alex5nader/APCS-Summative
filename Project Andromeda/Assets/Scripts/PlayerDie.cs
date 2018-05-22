8e6580c214104b0ff6788f1e349dbece6283ba98
habicha <habicha1@go.lisd.net>
2018-05-22 14:57:05 -0500

    Prettyfied score

--- Project Andromeda/Assets/Scripts/PlayerDie.cs
+++ Project Andromeda/Assets/Scripts/PlayerDie.cs
@@ -6,9 +6,9 @@ public class PlayerDie : MonoBehaviour {
 
     public void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("collision");
-        if (other.gameObject.CompareTag("Object"))
+        if (other.gameObject.CompareTag("Object")) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        else if (other.gameObject.CompareTag("Power Up"))
+        } else if (other.gameObject.CompareTag("Power Up"))
             throw new NotImplementedException();
     }
 }
