aaf74cf5bbdb8e1c8c45f60391edfd3155fad05f
liversquatch <sheldonm1@go.lisd.net>
2018-05-22 15:33:03 -0500

    5/22

--- Project Andromeda/Assets/Scripts/Reverse.cs
+++ Project Andromeda/Assets/Scripts/Reverse.cs
@@ -2,26 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour {
+public class Reverse : MonoBehaviour {
 
     private new Collider collider;
     public PearMan pearMan;
     public Scored scored;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         collider = GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
     private void OnTriggerEnter(Collision collision)
     {
         Debug.Log("collision");
-        scored.addPoints(100);
+        pearMan.reverse();
         Destroy(gameObject);
     }
 }
