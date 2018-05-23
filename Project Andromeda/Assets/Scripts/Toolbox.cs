6b5ab6a7a03fd67af97bbd2aa27994783e0f385a
habicha <habicha1@go.lisd.net>
2018-05-23 15:35:16 -0500

    Added max scroll speed

--- Project Andromeda/Assets/Scripts/Toolbox.cs
+++ Project Andromeda/Assets/Scripts/Toolbox.cs
@@ -3,14 +3,22 @@ public class Toolbox : Singleton<Toolbox> {
     protected Toolbox() { }
 
     public float ScrollSpeed { get; set; }
+    public float MaxScrollSpeed { get; set; }
     public float DefaultScrollSpeed { get; private set; }
+    public float ScrollDelta { get; set; }
 
     private void Awake() {
         DefaultScrollSpeed = 20f;
+        MaxScrollSpeed = 80f;
+        // number of frames in 5 seconds = fps * 5 seconds
+        // delta per frame = change in speed / (number of frames in 5 seconds)
+        ScrollDelta = (MaxScrollSpeed - DefaultScrollSpeed) / (60f * 60f * 5f);
         ScrollSpeed = DefaultScrollSpeed;
     }
 
     private void Update() {
-        ScrollSpeed *= 1.0005f;
+        ScrollSpeed += ScrollDelta;
+        if (ScrollSpeed > MaxScrollSpeed)
+            ScrollSpeed = MaxScrollSpeed;
     }
 }
