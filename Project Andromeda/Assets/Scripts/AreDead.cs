74e47281941a2c16e33536b986fdb9c52fe7c148
ThatOneHumann <rockycatal@hotmail.com>
2018-05-25 14:43:49 -0500

    Added 2p death

---
+++ Project Andromeda/Assets/Scripts/AreDead.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AreDead : MonoBehaviour {
+
+    private static int player;
+	// Use this for initialization
+	void Start () {
+        player = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+	}
+
+    public static void die()
+    {
+        player--;
+    }
+}
