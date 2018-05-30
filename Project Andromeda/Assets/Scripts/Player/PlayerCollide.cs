cf565881b8fe73c3d2fe88fe23495fc5daf77c1f
ThatOneHumann <rockycatal@hotmail.com>
2018-05-30 14:26:58 -0500

    e

--- Project Andromeda/Assets/Scripts/Player/PlayerCollide.cs
+++ Project Andromeda/Assets/Scripts/Player/PlayerCollide.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class PlayerCollide : MonoBehaviour {
 
     public bool isPear;
+    private Scored score;
+    public void Start()
+    {
+        score = GetComponent<Scored>();
+    }
 
     public void OnCollisionEnter2D(Collision2D other) {
+
         Toolbox.Instance.ScrollSpeed = Toolbox.Instance.DefaultScrollSpeed;
-        if (other.gameObject.tag.Contains("Object")) {
+        if (other.gameObject.tag.Contains("Object"))
+        {
             AreDead.die();
             Destroy(gameObject);
-        } else if (other.gameObject.CompareTag("Coin"))
-            throw new NotImplementedException();
+        }
+        else if (other.gameObject.CompareTag("Coin"))
+        {
+            score.addPoints(1000);
+            Destroy(other.gameObject);
+        }
     }
 }
